ad0c0e0e3986aeaa9f104e200bdfd862d309b432
Kolopox <bisson.th@gmail.com>
2018-05-07 13:25:03 +0200

    corrected bug on LED + WireVGND

--- Assets/Scripts/OnRun.cs
+++ Assets/Scripts/OnRun.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 //mellisa taylor
@@ -8,15 +7,25 @@ public class OnRun : MonoBehaviour {
 
     public GameObject light;
 
+    private List<List<int>> arrayOfSystems;
 
 	// Use this for initialization
 	void Start () {
-		
+        arrayOfSystems = new List<List<int>>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(arrayOfSystems.Count != 0)
+        {
+            //foreach(List<int> o in arrayOfSystems)
+            //{
+            //    foreach(int b in o)
+            //    {
+            //        Debug.Log(b);
+            //    }
+            //}
+        }
 	}
 
     public void ActiveRunMode()
@@ -24,10 +33,11 @@ public class OnRun : MonoBehaviour {
         light.GetComponent<Light>().enabled = false;
 
         GameObject[,] componentMap = gameObject.GetComponent<ControlInputs>().componentMap;
-        int tempI=0, tempJ=0;
+        int tempI=0, tempJ=0, saveTempI, saveTempJ;
         bool systemEnded = false;
-        bool firstTimeAfterBattery = true;
+        bool firstTimeAfterBattery;
         bool findOnlyNoComponent;
+        List<int> arrayOfActualSystem = new List<int>();
         for (int i = 0; i < componentMap.GetLength(0); i++)
         {
             for (int j = 0; j < componentMap.GetLength(1); j++)
@@ -36,8 +46,11 @@ public class OnRun : MonoBehaviour {
                 {
                     if (componentMap[i, j].GetComponent<IDIntoComponent>().identifier == MyComponents.Battery)
                     {
+                        firstTimeAfterBattery = true;
                         while (!systemEnded)
                         {
+                            saveTempI = tempI;
+                            saveTempJ = tempJ;
                             findOnlyNoComponent = true;
                             for (int k = 0; k < 4; k++)
                             {
@@ -88,6 +101,9 @@ public class OnRun : MonoBehaviour {
                                 if (componentMap[tempI, tempJ] != null)
                                 {
                                     findOnlyNoComponent = false;
+                                    arrayOfActualSystem.Add(tempI);
+                                    arrayOfActualSystem.Add(tempJ);
+
                                     if (componentMap[tempI, tempJ].GetComponent<IDIntoComponent>().identifier == MyComponents.wireVGND)
                                     {
                                         k = 3;
@@ -96,14 +112,16 @@ public class OnRun : MonoBehaviour {
                                     {
                                         componentMap[tempI, tempJ].GetComponent<Light>().enabled = true;
                                         systemEnded = true;
+                                        arrayOfSystems.Add(arrayOfActualSystem);
                                     }
                                     //componentMap[tempI, tempJ].GetComponent<Light>().enabled = true;
                                     //break;
                                 }
-                                if (k == 3 && !firstTimeAfterBattery)
-                                {
-                                    tempJ--;
-                                }
+                                //if (k == 3 && !firstTimeAfterBattery)
+                                //{
+                                //    tempI = saveTempI;
+                                //    tempJ = saveTempJ;
+                                //}
                                 if(k==3 && findOnlyNoComponent)
                                 {
                                     systemEnded = true;
